d111501049f5d2c7aa415a50805a800a1c7c144b
josuerx12 <josueaze12@gmail.com>
2024-04-11 14:43:35 -0300

    core(vehicle): Finish config DTO from vehicles;

--- src/vehicle/dto/create-vehicle.dto.ts
+++ src/vehicle/dto/create-vehicle.dto.ts
@@ -1,1 +1,9 @@
-export class CreateVehicleDto {}
+import { IsInt, IsString } from 'class-validator';
+import { Vehicle } from '../entities/vehicle.entity';
+
+export class CreateVehicleDto extends Vehicle {
+  @IsString({ message: 'Placa do veiculo é obrigatoria!' })
+  plate: string;
+  @IsInt({ message: 'Id do provedor dever ser um numero inteiro!' })
+  providerId: number;
+}
--- src/vehicle/entities/vehicle.entity.ts
+++ src/vehicle/entities/vehicle.entity.ts
@@ -1,1 +1,8 @@
-export class Vehicle {}
+import { Prisma } from '@prisma/client';
+
+export class Vehicle implements Prisma.VehicleUncheckedCreateInput {
+  plate: string;
+  providerId: number;
+  createdAt?: string | Date;
+  updatedAt?: string | Date;
+}
