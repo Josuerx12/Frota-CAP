8da814c972b14a2e541c04dfca7c87dfc8eab7ce
josuerx12 <josueaze12@gmail.com>
2024-04-17 15:58:40 -0300

    feat: new config.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,POST,PUT,PATCH ,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-  });
+  app.use(cors());
   await app.listen(3000);
 }
 bootstrap();
