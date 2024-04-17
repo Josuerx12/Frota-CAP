1dddb2a1421360dfd0b46c0a9e54092f233e4e3c
josuerx12 <josueaze12@gmail.com>
2024-04-17 15:50:03 -0300

    feat: new features

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
   });
   await app.listen(3000);
 }
