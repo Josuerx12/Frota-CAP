8dbc4d0ede5963bd3bb5af5ba8ce34528b55b297
josuerx12 <josueaze12@gmail.com>
2024-04-17 16:18:45 -0300

    core> new core config

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: [
-      'Content-Type',
-      'Origin',
-      'X-Requested-With',
-      'Accept',
-      'Authorization',
-    ],
-    exposedHeaders: ['Authorization'],
-    credentials: true,
-    preflightContinue: true,
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
