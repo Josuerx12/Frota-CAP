108655499f34960b4c059bc91f44cb15e3a00fe3
josuerx12 <josueaze12@gmail.com>
2024-04-19 14:58:06 -0300

    feat: all just fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    preflightContinue: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
