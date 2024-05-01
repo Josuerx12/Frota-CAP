27ff76aa72970086586404ea37fcd6e69587804a
josuerx12 <josueaze12@gmail.com>
2024-05-01 17:03:44 -0300

    feat: new features.

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(port);
+  await app.listen(port ? port : 3000);
 }
 bootstrap();
